97faa6a49ab797f985e5ab900be8b1410d331975
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-10-01 22:54:52 +0600

    courier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import Card from "./components/Card"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider}from "react-router-dom";
+import Rootlayout from "./components/Rootlayout";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Area from "./pages/Area";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Track from "./pages/Track";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+let router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Rootlayout/>}>
+    <Route index element={<Home/>}></Route>
+    <Route path="/price" element={<Pricing/>}></Route>
+    <Route path="/area" element={<Area/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
+    <Route path="/track" element={<Track/>}></Route>
+    <Route path="/signup" element={<Signup/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+  </Route>
+))
 
 function App() {
-
   return (
    <>
-    <Navbar/>
-    <Hero/>
-    <Card/>
+    <RouterProvider router={router} />
    </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Logo from "../assets/logo.svg";
 import { AiOutlineAim } from "react-icons/ai";
 import { FaBars } from "react-icons/fa6";
 import { ImCross } from "react-icons/im";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   let [show, setShow] = useState(false);
@@ -23,7 +24,7 @@ const Navbar = () => {
       <div className="container mx-auto">
         <div className="flex justify-between w-full">
           <div className="lg:w-1/5 flex items-center">
-            <img src={Logo} alt="Logo" />
+          <Link to="/"><img src={Logo} alt="Logo" /></Link>
           </div>
           <div className="w-4/5 flex justify-end items-center">
             <ul
@@ -35,25 +36,25 @@ const Navbar = () => {
             >
               <ImCross className='text-[20px] justify-end absolute top-3 right-3 lg:hidden cursor-pointer'/>
               <li className="text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal">
-                Pricing
+                <Link to="/price">Pricing</Link>
               </li>
               <li className="text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal">
-                Coverage
+                <Link to="/area">                Coverage</Link>
               </li>
               <li className="text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal">
-                About Us
+                <Link to="/about">About Us</Link>
               </li>
               <li className="text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal">
-                Contact
+                <Link to="/contact">Contact</Link>
               </li>
-              <li className=" flex items-center lg:ms-40 text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal gap-x-3 text-[#00b795]">
+              <Link to="/track"><li className=" flex items-center lg:ms-40 text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal gap-x-3 text-[#00b795]">
                 <AiOutlineAim /> <span>Track Parcel</span>
-              </li>
+              </li></Link>
               <li className="text-[20px] hover:text-[#00b795] duration-300 ease-in-out cursor-pointer font-pops font-normal">
-                <a href="#">Login</a>
+                <a href="#"><Link to="/login">Login</Link></a>
               </li>
               <li className="text-[20px] font-pops font-normal bg-[#00b795] px-10 py-5 rounded-md text-white">
-                <a href="#">Sign up</a>
+                <a href="#"><Link to="/signup">Sign up</Link></a>
               </li>
             </ul>
           </div>
